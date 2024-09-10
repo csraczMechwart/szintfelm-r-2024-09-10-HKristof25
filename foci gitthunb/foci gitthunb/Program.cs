e6d14879b6bc135833e922418a2b8527b1cff77a
Hajdu Kristóf <hajdu.kristof@burg.lo.hu>
2024-09-10 09:33:21 +0200

    Project created

---
+++ foci gitthunb/foci gitthunb/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace foci_gitthunb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
